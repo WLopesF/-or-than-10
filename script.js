66ce5f3c893b47dbaa699726e03b13daf13567ad
WLopesF <williamlopesfreitas789@gmail.com>
2024-11-09 12:12:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function maioroumenor():
+
+  let numero = prompt("Insira o seu número de escolha")
+
+if (numero < 10) {
+    alert("O seu número é menor que 10.")
+} else if (numero > 10) {
+    alert("O seu número é maior que 10.")
+} else {
+    alert("O seu número é igual a 10.")
+}
+
+  
